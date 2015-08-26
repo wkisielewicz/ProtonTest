2bdf416fd095370ffaaf5ade31fd8b494bbd48cd
wkisielewicz <wkisielwicz@criticue.com>
2015-08-26 22:47:59 +0200

    server drb with callbacks

---
+++ bin/server_test.rb
@@ -0,0 +1,42 @@
+require 'drb'
+require 'pathname'
+
+
+URI = 'druby://localhost:8989'
+$callback = nil
+
+class TestServer
+
+  def initialize
+    @branch = 'master'
+    project_root = Pathname.new(__FILE__).dirname.dirname
+    Dir.chdir(project_root)
+  end
+
+  def git_reset(&reset)
+    $callback = reset
+  end
+
+  def run_tests(&test)
+    $callback = test
+  end
+
+  def control_snapshot(&snapshot)
+    $calback = snapshot
+  end
+end
+
+FRONT_OBJECT = TestServer.new
+
+DRb.start_service(URI, FRONT_OBJECT)
+
+begin
+  loop do
+    $callback.call if $callback
+    sleep 1
+  end
+rescue DRb::DRbConnError
+  puts "client is gone, no connection"
+end
+
+DRb.thread.join

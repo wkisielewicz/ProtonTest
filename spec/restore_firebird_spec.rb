97bd8d639cbe6f9d3abd647248f2bdbebbc2a0dc
wkisielewicz <wkisielwicz@criticue.com>
2015-09-20 22:29:32 +0200

    change path to firebird

--- spec/restore_firebird_spec.rb
+++ spec/restore_firebird_spec.rb
@@ -5,25 +5,25 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 
-describe 'Backup Firebird', :type => :feature, :js => true do
+describe 'Restoring from a backup made using proton red', :type => :feature, :js => true do
 
-  it 'correct backup for firebird' do
+  it 'Proper restoration for a database firebird' do
 
     visit('http://localhost:10555/')
 
     page.find('button.btn-default.btn').click
     page.find('div.button-group > button.btn-primary.btn').click
-   # page.find('panel-body').text
-   #  fill_in 'main-view-restore-wizard-data-config-tools-gbak-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe'
-   #  expect(page).to have_field('main-view-restore-wizard-data-config-tools-gbak-path', with: 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe')
-   #  fill_in 'main-view-restore-wizard-data-config-tools-isql-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\isql.exe'
-    fill_in 'main-view-restore-wizard-data-config-database-connection-string',    :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
-    fill_in 'main-view-restore-wizard-data-config-database-login',                :with => 'SYSDBA'
-    fill_in 'main-view-restore-wizard-data-config-database-password',             :with => 'masterkey'
+    # page.find('panel-body').text
+    #  fill_in 'main-view-restore-wizard-data-config-tools-gbak-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe'
+    #  expect(page).to have_field('main-view-restore-wizard-data-config-tools-gbak-path', with: 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe')
+    #  fill_in 'main-view-restore-wizard-data-config-tools-isql-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\isql.exe'
+    fill_in 'main-view-restore-wizard-data-config-database-connection-string', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
+    fill_in 'main-view-restore-wizard-data-config-database-login', :with => 'SYSDBA'
+    fill_in 'main-view-restore-wizard-data-config-database-password', :with => 'masterkey'
     page.find('div.col-sm-7 > button.btn-primary.btn').click
     #
-    fill_in 'main-view-restore-wizard-data-config-passphrase1',      :with => 'test'
-    fill_in 'main-view-restore-wizard-data-config-passphrase2',      :with => 'test'
+    fill_in 'main-view-restore-wizard-data-config-passphrase1', :with => 'test'
+    fill_in 'main-view-restore-wizard-data-config-passphrase2', :with => 'test'
     page.find('div.col-sm-7 > button.btn-primary.btn').click
     page.find('#main-view-restore-wizard-accepted').click
     page.find('label > span').click

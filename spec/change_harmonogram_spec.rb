71ecc3220e6a03b1a0c44770baa88c9d74c44d80
wkisielewicz <wkisielwicz@criticue.com>
2015-09-20 22:31:03 +0200

    add new spec 'incorrect ' case

--- spec/change_harmonogram_spec.rb
+++ spec/change_harmonogram_spec.rb
@@ -5,15 +5,28 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 
-describe 'change harmonogram', :type => :feature, :js => true do
+describe 'change the schedule for automatic backup proton red', :type => :feature, :js => true do
 
-  it 'change harmonogram' do
+  before(:each) do
+
     visit('http://localhost:10555')
-      page.find('#config-schedule-monday').click
-      page.find('#config-schedule-tuesday').click
-      fill_in 'config-schedule-start-at',   :with => '15:00'
-      page.find('div.col-sm-7 > button.btn-primary.btn').click
-    page.driver.render('./screenshot/harmonogram.png', :full => true)
-    end
+
   end
+
+  it 'enter the correct value for the schedule' do
+    page.find('#config-schedule-monday').click
+    page.find('#config-schedule-tuesday').click
+    fill_in 'config-schedule-start-at', :with => '15:00'
+    page.find('div.col-sm-7 > button.btn-primary.btn').click
+    page.driver.render('./screenshot/harmonogram.png', :full => true)
+  end
+  it 'enter the incorrect value for the schedule' do
+    page.find('#config-schedule-monday').click
+    page.find('#config-schedule-tuesday').click
+    fill_in 'config-schedule-start-at', :with => '15;00'
+    page.find('div.col-sm-7 > button.btn-primary.btn').click
+    expect(page).to have_content 'format czasu'
+    page.driver.render('./screenshot/harmonogram1.png', :full => true)
+  end
+end
 

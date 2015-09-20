30c801283e1118ef4d13125137e920fb5351db59
wkisielewicz <wkisielwicz@criticue.com>
2015-09-20 17:18:13 +0200

    add create account proton-provision

--- spec/firebird_wizzard_spec.rb
+++ spec/firebird_wizzard_spec.rb
@@ -1,6 +1,8 @@
+require 'openssl'
 require 'rspec'
 require 'provision'
 require_relative 'spec_helper'
+require_relative 'protontest'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 
@@ -9,46 +11,47 @@ Capybara.javascript_driver = :poltergeist
 
 describe 'Setting up Firebird Wizzard', :type => :feature, :js => true do
 
-   before(:each) do
-     @account=Acount.create!(user_email,admin_email,licence)
+  before(:each) do
+
+    @key=Account.new().create!
+  end
+
+ after(:each) do
+    @id=Account.new().destroy!
+
  end
-after(:each) do
-  @account.destroy!
-
-end
   it 'should correct configuration firebird' do
-
-
-  @account.activation_key
 
     visit('http://localhost:10555/')
 
     #initial proton, setup acces key
     page.find('div.col-sm-7 > button.btn-primary.btn').click
-    fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with=> @account.activation.key
+    fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with => @key
     page.find('div.button-group > button.btn-primary.btn').click
+    sleep 5
+    page.driver.render('./screenshot/firebird_wizzard.png', :full => true)
     page.find('div.button-group > button.btn-primary.btn').click
     #security key
-    fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', :with=> 'test'
-    fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', :with=> 'test'
+    fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', :with => 'test'
+    fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', :with => 'test'
     page.find('div.button-group > button.btn-primary.btn').click
     page.find('div.panel-text > button.btn-primary.btn').click
     page.find('initial-wizard-setup-wizard-data-config-encryption-generate-understand').click
     page.find('initial-wizard-setup-wizard-data-config-encryption-generate-accepted').click
     page.find('div.button-group > button.btn-primary.btn').click
     #firebird settings tools
-    fill_in 'config-tools-gbak-path', :with=> 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe'
-    fill_in 'config-tools-isql-path', :with=> 'C:\Program Files\Firebird\Firebird_2_5\bin\isql.exe'
-    fill_in 'config-database-connection-string', :with=> 'C:\Program Files\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
-    fill_in 'config-database-login', :with=> 'SYSDBA'
-    fill_in 'config-database-password', :with=> 'masterkey'
+    fill_in 'config-tools-gbak-path', :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe'
+    fill_in 'config-tools-isql-path', :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\isql.exe'
+    fill_in 'config-database-connection-string', :with => 'C:\Program Files\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
+    fill_in 'config-database-login', :with => 'SYSDBA'
+    fill_in 'config-database-password', :with => 'masterkey'
     page.find('div.col-sm-7 > button.btn-primary.btn').click
     page.find('div.button-group > button.btn-primary.btn').click
     expect(page).to have_no_content 'Error'
-     page.driver.render('./screenshot/firebird_wizzard.png', :full => true)
+    page.driver.render('./screenshot/firebird_wizzard.png', :full => true)
 
   end
- end
+end
 
 
 

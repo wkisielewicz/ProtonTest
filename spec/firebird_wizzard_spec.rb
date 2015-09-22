6eb3f88f875b895e09bbcec09af16449c3b40d6d
wkisielewicz <wkisielwicz@criticue.com>
2015-09-22 16:03:46 +0200

    add download file method, setup firefox profile

--- spec/firebird_wizzard_spec.rb
+++ spec/firebird_wizzard_spec.rb
@@ -5,54 +5,75 @@ require_relative 'spec_helper'
 require_relative 'protontest'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
-
-
-Capybara.javascript_driver = :poltergeist
+require 'selenium-webdriver'
+require 'capybara/dsl'
 
 describe 'Setting up Firebird Wizzard', :type => :feature, :js => true do
-
   before(:each) do
 
     @account=Account.new().create!
 
   end
 
-  after(:each) do
-
-    @id = @account.subscription_id
-    system("proton-provision destroy -i #{@id}")
-  end
+   # after(:each) do
+   #
+   #   @id = @account.subscription_id
+   #   system("proton-provision destroy -i #{@id}")
+   # end
   it 'should correct configuration firebird' do
 
     @key = @account.activation_key
 
-    visit('http://localhost:10555/')
+    cb = Capybara
+
+    cb.register_driver :my_firefox_driver do |app|
+      profile = Selenium::WebDriver::Firefox::Profile.new
+      profile['browser.download.dir'] = "~/Downloads"
+      profile['browser.download.folderList'] = 2
+      profile['browser.helperApps.alwaysAsk.force'] = false
+      profile['browser.download.manager.showWhenStarting'] = false
+      profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/octet-stream'
+      profile['csvjs.disabled'] = true
+      Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+    end
+
+    cb.current_driver = :my_firefox_driver
+
+
+    cb.visit('http://localhost:10555/')
 
     #initial proton, setup acces key
-    page.find('div.col-sm-7 > button.btn-primary.btn').click
-    fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with => @key
-    page.find('div.button-group > button.btn-primary.btn').click
+    cb.page.find('div.col-sm-7 > button.btn-primary.btn').click
+    fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with =>  @key
+    cb.page.find('div.button-group > button.btn-primary.btn').click
     sleep 5
-    # page.driver.render('./screenshot/firebird_wizzard.png', :full => true)
-    page.find('div.button-group > button.btn-primary.btn').click
+    cb.page.find('div.button-group > button.btn-primary.btn').click
     #enter the password for the file rescue
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', :with => 'test'
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', :with => 'test'
-    page.find('div.button-group > button.btn-primary.btn').click
-    page.find('div.panel-text > button.btn-primary.btn').click
-    page.find('initial-wizard-setup-wizard-data-config-encryption-generate-understand').click
-    page.find('initial-wizard-setup-wizard-data-config-encryption-generate-accepted').click
-    page.find('div.button-group > button.btn-primary.btn').click
+    cb.page.find('div.button-group > button.btn-primary.btn').click
+    cb.page.find('div.panel-text > button.btn-primary.btn').click
+    sleep 13
+    #page.driver.render('./screenshot/firebird_wizard.png', :full => true)
+    cb.page.find('#initial-wizard-setup-wizard-data-config-encryption-generate-understand').click
+    cb.page.find('#initial-wizard-setup-wizard-data-config-encryption-generate-accepted').click
+    cb.page.find('div.button-group > button.btn-primary.btn').click
+    sleep 5
     #typing paths to the database tools firebird
-    fill_in 'config-tools-gbak-path', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\gbak.exe'
-    fill_in 'config-tools-isql-path', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\isql.exe'
-    fill_in 'config-database-connection-string', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
-    fill_in 'config-database-login', :with => 'SYSDBA'
-    fill_in 'config-database-password', :with => 'masterkey'
-    page.find('div.col-sm-7 > button.btn-primary.btn').click
-    page.find('div.button-group > button.btn-primary.btn').click
+    # fill_in 'config-tools-gbak-path', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\gbak.exe'
+    # fill_in 'config-tools-isql-path', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\isql.exe'
+    # expect(page).to have_field('config-tools-gbak-path', with: 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\gbak.exe')
+    # expect(page).to have_field('config-tools-isql-path', with: 'C:\Program Files (x86)\Firebird\Firebird_2_5\bin\isql.exe')
+    #cb.page.driver.render('./screenshot/firebird_wizard1.png', :full => true)
+    fill_in 'initial-wizard-setup-wizard-data-config-database-connection-string', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
+    fill_in 'initial-wizard-setup-wizard-data-config-database-login', :with => 'SYSDBA'
+    fill_in 'initial-wizard-setup-wizard-data-config-database-password', :with => 'masterkey'
+    ## page.find('div.col-sm-7 > button.btn-primary.btn').click
+    cb.page.find('div.button-group > button.btn-primary.btn').click
+    cb.page.find('div.button-group > button.btn-primary.btn').click
+    sleep(inspection_time=5)
     # expect(page).to have_no_content 'Error'
-    page.driver.render('./screenshot/firebird_wizzard.png', :full => true)
+    #cb.page.driver.render('./screenshot/firebird_wizard.png', :full => true)
 
   end
 end

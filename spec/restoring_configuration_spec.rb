beb268b53986df877969ebcfb540f00b76ff7cb4
wkisielewicz <wkisielwicz@criticue.com>
2015-09-28 21:40:39 +0200

    spec that restores the configuration file rescue

---
+++ spec/restoring_configuration_spec.rb
@@ -0,0 +1,32 @@
+require 'openssl'
+require 'rspec'
+require 'provision'
+require_relative 'spec_helper'
+require_relative 'protontest'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+require 'selenium-webdriver'
+require 'capybara/dsl'
+
+
+Capybara.javascript_driver = :poltergeist
+
+describe 'Restoring configuration ',  :type => :feature, :js => true do
+
+  it 'restoring configuration using rescue file' do
+    visit('http://localhost:10555')
+    find(:xpath, "(//input[@id='initial-wizard-wizard-mode'])[2]").click
+    page.find('div.col-sm-7 > button.btn-primary.btn').click
+    sleep 5
+    fill_in 'initial-wizard-setup-wizard-data-config-passphrase1', :with => 'test'
+    fill_in 'initial-wizard-setup-wizard-data-config-passphrase2', :with => 'test'
+    page.find('div.col-sm-7 > button.btn-primary.btn').click
+    sleep 4
+    attach_file('file', 'C:\\Users\\kisiel\\Downloads\\plik-ratunkowy.prcv')
+    sleep 4
+    page.find('div.button-group > button.btn-primary.btn').click
+    page.find('div.button-group > button.btn-primary.btn').click
+    sleep 5
+    page.driver.render('./screenshot/restoring_configuration_rescue_file.png', :full => true)
+  end
+end

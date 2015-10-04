4be5b07477adeb3b34547d36744c9ee014ef67af
wkisielewicz <wkisielwicz@criticue.com>
2015-10-04 17:21:23 +0200

    change spec to Proton 0.9.15

--- spec/restore_firebird_spec.rb
+++ spec/restore_firebird_spec.rb
@@ -2,10 +2,13 @@ require 'rspec'
 require_relative 'spec_helper'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+require 'protontest'
 
 Capybara.javascript_driver = :poltergeist
 
 describe 'Restoring from a backup made using proton red', :type => :feature, :js => true do
+
+  let(:fb) {Firebird_Variables.new(:gbak_path,:isql_path,:full_path,:login,:password,:wrong_password,:security_password)}
 
   it 'Proper restoration for a database firebird' do
 
@@ -18,26 +21,34 @@ describe 'Restoring from a backup made using proton red', :type => :feature, :js
     #  fill_in 'main-view-restore-wizard-data-config-tools-gbak-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe'
     #  expect(page).to have_field('main-view-restore-wizard-data-config-tools-gbak-path', with: 'C:\Program Files\Firebird\Firebird_2_5\bin\gbak.exe')
     #  fill_in 'main-view-restore-wizard-data-config-tools-isql-path',               :with => 'C:\Program Files\Firebird\Firebird_2_5\bin\isql.exe'
-    fill_in 'main-view-restore-wizard-data-config-database-connection-string', :with => 'C:\Program Files (x86)\Firebird\Firebird_2_5\examples\empbuild\EMPLOYEE.FDB'
-    fill_in 'main-view-restore-wizard-data-config-database-login', :with => 'SYSDBA'
-    fill_in 'main-view-restore-wizard-data-config-database-password', :with => 'masterkey'
+    fill_in 'main-view-restore-wizard-data-config-database-connection-string', with: fb.full_path
+    fill_in 'main-view-restore-wizard-data-config-database-login', with: fb.login
+    fill_in 'main-view-restore-wizard-data-config-database-password', with: fb.password
     page.find('div.col-sm-7 > button.btn-primary.btn').click
+
     #
-    fill_in 'main-view-restore-wizard-data-config-passphrase1', :with => 'test'
-    fill_in 'main-view-restore-wizard-data-config-passphrase2', :with => 'test'
+    fill_in 'main-view-restore-wizard-data-config-passphrase1', with: fb.security_password
+    fill_in 'main-view-restore-wizard-data-config-passphrase2', with: fb.security_password
     page.find('div.col-sm-7 > button.btn-primary.btn').click
-    page.find('#main-view-restore-wizard-accepted').click
-    page.find('label > span').click
+    page.driver.render('./screenshot/firebird_restore.png', :full => true)
     attach_file('file', 'C:\\Users\\kisiel\\Downloads\\plik-ratunkowy.prcv')
+    page.find('div.button-group > button.btn-primary.btn').click
+    sleep 5
+    page.find('div.button-group > button.btn-primary.btn').click
 
-    sleep(inspection_time=8)
-    page.find('#main-view-restore-wizard-accepted').click
-    sleep(inspection_time=5)
+    sleep 5
+    page.find('#main-view-restore-wizard-safety-copy').click
+    sleep 5
+    page.find('label > span').click
 
     page.find('div.button-group > button.btn-primary.btn').click
+    sleep 4
+    #page.find('#main-view-restore-wizard-accepted').click
+    find(:xpath, "//input[@id='main-view-restore-wizard-accepted']").click
+    sleep 5
     page.find('div.button-group > button.btn-primary.btn').click
     page.find('div.button-group > button.btn-primary.btn').click
-    page.find('div.button-group > button.btn-primary.btn').click
+    sleep 3
     page.driver.render('./screenshot/firebird_restore.png', :full => true)
   end
 end

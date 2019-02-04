cc16caa31c970f2956c3a97f0008fd04d7b9e05a
Wioletta Kisielewicz <kisielewicz777@gmail.com>
2019-02-04 21:53:34 +0100

    Removed sleeps.

--- spec/proton_initial_backup_restore_spec.rb
+++ spec/proton_initial_backup_restore_spec.rb
@@ -45,26 +45,20 @@ describe 'Setting up Firebird Wizzard', :type => :feature, :js => true do
     cb.page.find('div.col-sm-7 > button.btn-primary.btn').click
     fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with => @key
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 6
     cb.page.find('div.button-group > button.btn-primary.btn').click
     fill_in 'initial-wizard-setup-wizard-data-config-database-connection-string', with: fb.full_path
     fill_in 'initial-wizard-setup-wizard-data-config-database-login', with: fb.login
-    sleep 4
     fill_in 'initial-wizard-setup-wizard-data-config-database-password', with: fb.password_firebird_database
-    sleep 5
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 3
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', with: fb.security_password
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', with: fb.security_password
     cb.page.find('div.button-group > button.btn-primary.btn').click
     cb.page.find('div.panel-text > button.btn-primary.btn').click
-    sleep 13
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 3
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 8
     puts cb.title
   end
+  
   ################backup###############################
 
   Capybara.javascript_driver = :poltergeist
@@ -99,52 +93,32 @@ describe 'Setting up Firebird Wizzard', :type => :feature, :js => true do
 
       page.find('button.btn-default.btn').click
       page.find('div.button-group > button.btn-primary.btn').click
-      sleep 5
-      # page.find('panel-body').text
-      #  fill_in 'main-view-restore-wizard-data-config-tools-gbak-path',               with: fb.gbak_path
-      #  expect(page).to have_field('main-view-restore-wizard-data-config-tools-gbak-path', with: fb.gabk_path
-      #  fill_in 'main-view-restore-wizard-data-config-tools-isql-path',               with: fb.isql_path
       fill_in 'main-view-restore-wizard-data-config-database-connection-string', with: fb.full_path
       fill_in 'main-view-restore-wizard-data-config-database-login', with: fb.login
       fill_in 'main-view-restore-wizard-data-config-database-password', with: fb.password_firebird_database
       page.find('div.col-sm-7 > button.btn-primary.btn').click
-
-      #
       fill_in 'main-view-restore-wizard-data-config-passphrase1', with: fb.security_password
       fill_in 'main-view-restore-wizard-data-config-passphrase2', with: fb.security_password
       page.find('div.col-sm-7 > button.btn-primary.btn').click
       page.driver.render('./screenshot/firebird_restore.png', :full => true)
       attach_file('file', 'C:\\Users\\kisiel\\Downloads\\plik-ratunkowy.prcv')
       page.find('div.button-group > button.btn-primary.btn').click
-      sleep 5
       page.find('div.button-group > button.btn-primary.btn').click
-
-      sleep 5
       page.find('#main-view-restore-wizard-safety-copy').click
-      sleep 5
       page.find('label > span').click
-
       page.find('div.button-group > button.btn-primary.btn').click
-      sleep 4
-      #page.find('#main-view-restore-wizard-accepted').click
       find(:xpath, "//input[@id='main-view-restore-wizard-accepted']").click
-      sleep 5
       page.find('div.button-group > button.btn-primary.btn').click
       page.find('div.button-group > button.btn-primary.btn').click
-      sleep 3
       page.driver.render('./screenshot/firebird_restore.png', :full => true)
-
-      @id = $account.subscription_id
-      system("proton-provision destroy -i #{@id}")
     end
 
   end
 end
 
 
-##########################################
-# after(:each) do
-#
-#   @id = @account.subscription_id
-#   system("proton-provision destroy -i #{@id}")
-# end
+ after(:each) do
+
+   @id = @account.subscription_id
+   system("proton-provision destroy -i #{@id}")
+ end

18061b1ee415b3460d315df3d28159f8e007e7f3
Wioletta Kisielewicz <kisielewicz777@gmail.com>
2019-02-04 21:50:44 +0100

    Removed sleep.

--- spec/generate_security_key_spec.rb
+++ spec/generate_security_key_spec.rb
@@ -36,22 +36,17 @@ describe 'generate security key', :type => :feature, :js => true do
     cb.current_driver = :my_firefox_driver
     cb.visit('http://localhost:10555/')
     cb.find(:xpath, "//div[@id='app']/div/div[2]/div/ul/li[3]/a").click
-    #cb.page.find('li.tab-danger. > a').click # znajdz nowy selektor
     cb.page.find('div.tab-contents > button.btn-primary.btn').click
     fill_in 'admin-dashboard-encryption-wizard-data-config-encryption-passphrase1', with: fb.security_password
     fill_in 'admin-dashboard-encryption-wizard-data-config-encryption-passphrase2', with: fb.security_password
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 5
     cb.page.find('#admin-dashboard-encryption-wizard-data-config-encryption-generate-understand').click
-
-    #cb.page.find('label > strong').click
     cb.page.find('#admin-dashboard-encryption-wizard-data-config-encryption-generate-accepted').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 5
     cb.page.find('div.panel-text > button.btn-primary.btn').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    #cb.page.driver.render('./screenshot/generate_security_key.png', :full => true)
+    cb.page.driver.render('./screenshot/generate_security_key.png', :full => true)
 
   end
 end

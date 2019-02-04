9ffdd5ce654daeaaffc234f0c8c7a65680eda6af
Wioletta Kisielewicz <kisielewicz777@gmail.com>
2019-02-04 21:49:24 +0100

    Code refactoring.

--- spec/firebird_wizard_spec.rb
+++ spec/firebird_wizard_spec.rb
@@ -17,11 +17,11 @@ describe 'Setting up Firebird Wizard', :type => :feature, :js => true do
 
   end
 
-  # after(:each) do
-  #
-  #   @id = @account.subscription_id
-  #   system("proton-provision destroy -i #{@id}")
-  # end
+   after(:each) do
+  
+     @id = @account.subscription_id
+     system("proton-provision destroy -i #{@id}")
+   end
   it 'should correct configuration firebird' do
 
     @key = @account.activation_key
@@ -48,34 +48,32 @@ describe 'Setting up Firebird Wizard', :type => :feature, :js => true do
     cb.page.find('div.col-sm-7 > button.btn-primary.btn').click
     fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with => @key
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 5
     cb.page.find('div.button-group > button.btn-primary.btn').click
+    
     #enter the password for the file rescue
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', with: fb.security_password
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', with: fb.security_password
     cb.page.find('div.button-group > button.btn-primary.btn').click
     cb.page.find('div.panel-text > button.btn-primary.btn').click
-    sleep 13
-    #page.driver.render('./screenshot/firebird_wizard.png', :full => true)
+    page.driver.render('./screenshot/firebird_wizard.png', :full => true)
     cb.page.find('#initial-wizard-setup-wizard-data-config-encryption-generate-understand').click
     cb.page.find('#initial-wizard-setup-wizard-data-config-encryption-generate-accepted').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 5
+    
     #typing paths to the database tools firebird
-    # fill_in 'config-tools-gbak-path', with: 'fb.gbak_path
-    # fill_in 'config-tools-isql-path', with: fb.isql_path
-    # expect(page).to have_field('config-tools-gbak-path', with: fb.gbak_path
-    # expect(page).to have_field('config-tools-isql-path', with: fb.isql_path
+    fill_in 'config-tools-gbak-path', with: 'fb.gbak_path
+    fill_in 'config-tools-isql-path', with: fb.isql_path
+    expect(page).to have_field('config-tools-gbak-path', with: fb.gbak_path
+    expect(page).to have_field('config-tools-isql-path', with: fb.isql_path
     #cb.page.driver.render('./screenshot/firebird_wizard1.png', :full => true)
     fill_in 'initial-wizard-setup-wizard-data-config-database-connection-string', with: fb.full_path
     fill_in 'initial-wizard-setup-wizard-data-config-database-login', with: fb.login
     fill_in 'initial-wizard-setup-wizard-data-config-database-password', with: fb.password
-    ## page.find('div.col-sm-7 > button.btn-primary.btn').click
+    page.find('div.col-sm-7 > button.btn-primary.btn').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep(inspection_time=5)
-    # expect(page).to have_no_content 'Error'
-    #cb.page.driver.render('./screenshot/firebird_wizard.png', :full => true)
+    expect(page).to have_no_content 'Error'
+    cb.page.driver.render('./screenshot/firebird_wizard.png', :full => true)
 
   end
 end

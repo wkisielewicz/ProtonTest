e916a816c87280e968208da7fb618ee2df36056e
Wioletta Kisielewicz <kisielewicz777@gmail.com>
2019-02-04 21:47:02 +0100

    Removed sleeps.

--- spec/create_and_restore_config_using_rescue_file_spec.rb
+++ spec/create_and_restore_config_using_rescue_file_spec.rb
@@ -44,21 +44,16 @@ describe 'Setting up Firebird Wizard', :type => :feature, :js => true do
     cb.page.find('div.col-sm-7 > button.btn-primary.btn').click
     fill_in 'initial-wizard-setup-wizard-data-user-access-token', :with => @key
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 6
     cb.page.find('div.button-group > button.btn-primary.btn').click
     fill_in 'initial-wizard-setup-wizard-data-config-database-connection-string', with: fb.full_path
     fill_in 'initial-wizard-setup-wizard-data-config-database-login', with: fb.login
     fill_in 'initial-wizard-setup-wizard-data-config-database-password', with: fb.password_firebird_database
-    sleep 5
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep 3
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase1', with: fb.security_password
     fill_in 'initial-wizard-setup-wizard-data-config-encryption-passphrase2', with: fb.security_password
     cb.page.find('div.button-group > button.btn-primary.btn').click
     cb.page.find('div.panel-text > button.btn-primary.btn').click
-    sleep 13
     cb.page.find('div.button-group > button.btn-primary.btn').click
-    sleep
     cb.page.find('div.button-group > button.btn-primary.btn').click
 
   end
@@ -74,17 +69,13 @@ describe 'Setting up Firebird Wizard', :type => :feature, :js => true do
       visit('http://localhost:10555')
       find(:xpath, "(//input[@id='initial-wizard-wizard-mode'])[2]").click
       page.find('div.col-sm-7 > button.btn-primary.btn').click
-      sleep 5
       fill_in 'initial-wizard-setup-wizard-data-config-passphrase1', with: fb.security_password
       fill_in 'initial-wizard-setup-wizard-data-config-passphrase2', with: fb.security_password
       page.find('div.col-sm-7 > button.btn-primary.btn').click
-      sleep 4
-      attach_file('file', 'C:\\Users\\kisiel\\Downloads\\plik-ratunkowy.prcv')
-      sleep 4
+      attach_file('file', 'C:\\plik-ratunkowy.prcv')
       page.find('div.button-group > button.btn-primary.btn').click
       page.find('div.button-group > button.btn-primary.btn').click
       page.find('div.button-group > button.btn-primary.btn').click
-      sleep 5
       page.driver.render('./screenshot/restoring_configuration_rescue_file.png', :full => true)
 
       @id = $account.subscription_id
